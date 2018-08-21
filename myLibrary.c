6abe1a8fb3b8f9784fa7cfb6dc8ecda391091170
Anthony Lee <antwonlee@gmail.com>
2018-08-20 22:33:17 -0500

    Add preprocessor commands

---
+++ myLibrary.c
@@ -0,0 +1,7 @@
+void function1(void){
+  puts("It works :)");
+}
+
+void function2(void){
+  // This function does nothing as well
+}
---
+++ preprocessor-commands/02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "myLibrary.h"
+
+#define MAX_LENGTH 21.8
+#define WORK_WEEK 5
+
+int main(void) {
+  function1();
+  return EXIT_SUCCESS;
+}
+
